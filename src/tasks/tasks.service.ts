2042687f7eea45035a871558a530a8fdc97ec781
vanessa pellegrini <vanessapellegrini@hotmail.com>
2022-08-23 23:50:14 -0400

    wip search and filter
    Signed-off-by: vanessa pellegrini <vanessapellegrini@hotmail.com>

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,17 @@ export class TasksService {
 
   getAlltasks() {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+    //temporary array
+    const tasks = this.getAlltasks();
+    if (status) {
+    }
+    if (search) {
+    }
+    return tasks;
   }
 
   getTaskById(id: string): Task {
