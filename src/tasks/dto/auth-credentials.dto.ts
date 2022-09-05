9bcd6586adce7b8a492363fbdb7747c54f14c0ac
vanessa pellegrini <vanessapellegrini@hotmail.com>
2022-09-05 02:00:46 -0400

    credentials and passwords validations
    Signed-off-by: vanessa pellegrini <vanessapellegrini@hotmail.com>

--- src/tasks/dto/auth-credentials.dto.ts
+++ src/tasks/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(6)
+  @MaxLength(10)
   username: string;
+
+  @MinLength(8)
+  @MaxLength(12)
+  @Matches(
+    /(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    { message: 'password is too week' },
+  )
   password: string;
 }
